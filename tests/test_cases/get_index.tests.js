38e5dd06cba5650cbe77dafab0d82dba7ec3fef0
cjjohansen <chjo@danskkabeltv.dk>
2020-05-16 20:42:37 +0200

    Acceptance tests working and circle ci configured

--- tests/test_cases/get_index.tests.js
+++ tests/test_cases/get_index.tests.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const when = require('../steps/when')
 const { init } = require('../steps/init')
 
-console.log = jest.fn();
+//console.log = jest.fn();
 
 describe(`When we invoke the GET / endpoint`, () => {
   beforeAll(async () => await init())
@@ -11,8 +11,10 @@ describe(`When we invoke the GET / endpoint`, () => {
   it(`Should return the index page with 8 restaurants`, async () => {
     const res = await when.we_invoke_get_index()
 
+    console.log('headers:',res.headers)
+
     expect(res.statusCode).to.equal(200)
-    expect(res.headers['Content-Type']).to.equal('text/html; charset=UTF-8')
+    expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8')
     expect(res.body).to.not.be.null
 
     const $ = cheerio.load(res.body)
